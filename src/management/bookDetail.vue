a9266b316f02abdae22909598c8f6be9be41579e
Yang Sihang <316699413@qq.com>
2020-03-17 22:50:05 +0800

    修改书目

--- src/management/bookDetail.vue
+++ src/management/bookDetail.vue
@@ -23,8 +23,63 @@
             </el-col>
         </el-row>
         <el-row>
-            <el-col :span="20" :offset="2">
-                <el-button class="btn">编辑</el-button>
+            <el-col :span="20" :offset="2" v-for="one in book" :key="one.uuid">
+                <el-button class="btn" @click="showEdit(one)">编辑</el-button>
+
+                <el-dialog title="编辑内容" :visible.sync="dialogFormVisible" @closed="reloadPage">
+                    <el-form :model="changeInfo">
+                        <el-form-item label="商品名称：" :label-width="formLabelWidth">
+                            <el-input v-model="changeInfo.name" autocomplete="off"></el-input>
+                        </el-form-item>
+                        <el-form-item label="作者：" :label-width="formLabelWidth">
+                            <el-input v-model="changeInfo.author" autocomplete="off"></el-input>
+                        </el-form-item>
+                        <el-form-item label="出版社：" :label-width="formLabelWidth">
+                            <el-input v-model="changeInfo.press" autocomplete="off"></el-input>
+                        </el-form-item>
+                        <el-form-item label="类别：" :label-width="formLabelWidth">
+                            <el-select v-model="changeInfo.sort" placeholder="请选择类别">
+                            <el-option label="小说" value="novel"></el-option>
+                            <el-option label="育儿" value="parenting"></el-option>
+                            <el-option label="艺术" value="art"></el-option>
+                            <el-option label="历史" value="history"></el-option>
+                            <el-option label="教辅" value="education"></el-option>
+                            <el-option label="管理" value="management"></el-option>
+                            <el-option label="哲学" value="philosophy"></el-option>
+                            <el-option label="科技" value="technology"></el-option>
+                            <el-option label="健康" value="health"></el-option>
+                            </el-select>
+                        </el-form-item>
+                        <el-form-item label="价格：" :label-width="formLabelWidth">
+                            <el-input v-model="changeInfo.price" autocomplete="off"></el-input>
+                        </el-form-item>
+                        <el-form-item label="库存：" :label-width="formLabelWidth">
+                            <el-input v-model="changeInfo.stock" autocomplete="off"></el-input>
+                        </el-form-item>
+                        <el-form-item label="描述：" :label-width="formLabelWidth">
+                            <el-input v-model="changeInfo.description" autocomplete="off"></el-input>
+                        </el-form-item>
+                        <!-- <el-form-item label="商品图片：" :label-width="formLabelWidth">
+                            <el-upload
+                                class="upload-demo"
+                                action="https://jsonplaceholder.typicode.com/posts/"
+                                :on-preview="handlePreview"
+                                :on-remove="handleRemove"
+                                :before-remove="beforeRemove"
+                                multiple
+                                :limit="1"
+                                :on-exceed="handleExceed"
+                                :file-list="changeInfo.image">
+                                <el-button size="small" type="primary">点击上传</el-button>
+                                <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+                            </el-upload>
+                        </el-form-item> -->
+                    </el-form>
+                    <div slot="footer" class="dialog-footer">
+                        <el-button @click="dialogFormVisible = false">取 消</el-button>
+                        <el-button type="primary" @click="editBook()">确 定</el-button>
+                    </div>
+                </el-dialog>
             </el-col>
         </el-row>
     </div>
@@ -34,11 +89,51 @@ export default {
     name:'bookDetail',
     data(){
         return{
+            dialogFormVisible: false,
+            formLabelWidth: '120px',
             book:[],
             changeInfo:{
-                name:''
+                name:'',
+                author:'',
+                press:'',
+                sort:'',
+                price:'',
+                stock:'',
+                description:'',
+                image:'',
             }
         }
+    },
+    methods:{
+        showEdit(one){
+            this.dialogFormVisible = true;
+            this.changeInfo = one;
+        },
+        editBook(){
+            this.axios.put('/books/aBook'+this.$route.params.id)
+                .then(function (response) {
+                    console.log(response);
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+            this.reloadPage;
+            this.dialogFormVisible = false;
+        },
+        reloadPage(){
+            var that = this;
+            console.log(this.$route.params.id)
+            this.axios.get('/books/aBook/'+this.$route.params.id)
+                .then(function (response) {
+                    console.log(response);
+                    let res = response.data;
+                    that.book = res.data;
+                    console.log(that.book)
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        },
     },
     mounted:function(){
         console.log(this.$route.params.id)
